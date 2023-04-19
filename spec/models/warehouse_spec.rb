44c55dbc0e6cc6e0e0951a89f6decd6d4a2a58c0
debicky <debickiart@gmail.com>
2023-04-19 06:39:17 +0200

    add warehouse spec

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe Warehouse, type: :model do
+  let(:warehouse) { build(:warehouse) }
+
+  describe 'associations' do
+    it 'has many stocks' do
+      warehouse = Warehouse.reflect_on_association(:stocks)
+      expect(warehouse.macro).to eq(:has_many)
+    end
+  end
+
+  describe 'validations' do
+    it 'validates presence of code' do
+      warehouse.code = nil
+      expect(warehouse.valid?).to eq(false)
+      expect(warehouse.errors[:code]).to include("can't be blank")
+    end
+  end
+end
