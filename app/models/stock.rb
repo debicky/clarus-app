8c5a33d26800e567115a3787b3054782e9d3f843
debicky <debickiart@gmail.com>
2023-04-28 06:49:54 +0200

    add brakeman

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,7 @@ class Stock < ApplicationRecord
   belongs_to :warehouse
   belongs_to :product
   has_many :orders
-  # add dependencies
+  # add dependencies?
 
   validates :warehouse, presence: true
   validates :product, presence: true
